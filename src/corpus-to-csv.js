6b1389bbfbdaa02ae7a4ad226d4a096b9be2df4a
Nick Freear <nfreear@yahoo.co.uk>
2020-10-03 22:54:30 +0100

    feat: `corpus-to-csv` now runs for all corpora (#12)

--- src/corpus-to-csv.js
+++ src/corpus-to-csv.js
@@ -7,41 +7,56 @@
 const fs = require('fs');
 const { join } = require('path');
 
-const CORPUS_JSON = join(__dirname, '..', 'corpus-en.json');
-const CORPUS_CSV = CORPUS_JSON + '.csv';
+const CORPORA = require('../conf.json').settings.nlp.corpora;
+const COMMA = ',';
 
-try {
-  corpusToCsv(CORPUS_JSON, CORPUS_CSV);
-} catch (err) {
-  console.error('ERROR', err);
-}
+console.warn('Corpora:', CORPORA);
 
-async function corpusToCsv (jsonFilePath = CORPUS_JSON, csvFilePath = CORPUS_CSV) {
-  const csvData = [ 'Intent,Utterance,Answer,Opts,' ];
+CORPORA.forEach((corpus, idx) => {
+  const CORPUS_JSON = join(__dirname, '..', corpus);
+  const CORPUS_CSV = CORPUS_JSON + '.csv';
+
+  try {
+    corpusToCsv(CORPUS_JSON, CORPUS_CSV, idx + 1);
+  } catch (err) {
+    console.error('ERROR', err);
+  }
+});
+
+async function corpusToCsv (jsonFilePath, csvFilePath, idx) {
+  const sep = COMMA;
+  const csvData = [`ID${sep}Intent${sep}Utterance${sep}Answer${sep}Opts${sep}X-Pinyin${sep}`];
 
   const JSON_DATA = await fs.promises.readFile(jsonFilePath, 'utf8');
   const CORPUS = JSON.parse(JSON_DATA);
 
   CORPUS.data.forEach(item => {
     const INTENT = item.intent;
+    const ID = item.id || '';
 
-    item.utterances.forEach(utterance => { csvData.push(`${INTENT},"${utterance}",,`); });
+    item.utterances.forEach(utterance => { csvData.push(`${ID}${sep}${INTENT}${sep}"${utterance}"${sep}${sep}`); });
 
     if (item.answers) {
       item.answers.forEach(answer => {
         if (typeof answer === 'string') {
-          csvData.push(`${INTENT},,"${answer}",`);
+          csvData.push(`${ID}${sep}${INTENT}${sep}${sep}"${answer}"${sep}`);
         } else {
-          csvData.push(`${INTENT},,"${answer.answer}",${answer.opts || ''},`)
+          csvData.push(`${ID}${sep}${INTENT}${sep}${sep}"${answer.answer}"${sep}${answer.opts || ''}${sep}`);
         }
+      });
+    }
+
+    if (item['x-pinyin']) {
+      item['x-pinyin'].forEach(pinyin => {
+        csvData.push(`${ID}${sep}${INTENT}${sep}${sep}${sep}"${pinyin}"`);
       });
     }
   });
 
   await fs.promises.writeFile(csvFilePath, csvData.join('\n'));
 
-  console.warn('Input file:     ', jsonFilePath);
-  console.warn('Locale:         ', CORPUS.locale);
-  console.warn('Count intents:  ', CORPUS.data.length);
-  console.warn('Count CSV lines:', csvData.length);
+  console.warn(`${idx}. Input file:     `, jsonFilePath);
+  console.warn('   Locale:         ', CORPUS.locale);
+  console.warn('   Count intents:  ', CORPUS.data.length);
+  console.warn('   Count CSV lines:', csvData.length);
 }
